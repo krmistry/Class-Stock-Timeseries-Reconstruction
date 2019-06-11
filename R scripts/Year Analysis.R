76c8d44c3aa3c2cf43641879cbfa8d545769d3fe
Kelly Mistry <kelly.r.mistry@gmail.com>
2019-06-11 11:56:31 -0700

    Begin analysis

--- R scripts/Year Analysis.R
+++ R scripts/Year Analysis.R
@@ -4,11 +4,25 @@
 
 stock_csv <- read.csv("stock.csv")
 
+timeseries <- read.csv("timeseries.csv")
+
 indices <- c(grep("Australia Rock Lobster", temp$stocklong), grep("Australia Rock Lobster", temp$stocklong))
 
 stockid_of_interest <- c("COD2J3KL", "SARDPCOAST", "PANCHPERUNC", "PHALNPAC", 
                          "HERRNORSS", "WPOLLEBS", "PILCHPJPN", "ATBTUNAEATL")
 
+
+classic_stock_timeseries_list <- lapply(stockid_of_interest, function(x) {
+  timeseries[timeseries$stockid == x, ]
+  })
+
+summary_data <- lapply(classic_stock_timeseries_list, function(x) {
+  number_of_assessments <- length(unique(x$assessid))
+  year_min <- min(x$tsyear)
+  year_max <- max(x$tsyear)
+  return(list(number_of_assessments = number_of_assessments, 
+              year_min = year_min, year_max = year_max))
+})
 
 # Unclear stockids:
 # Bristol Bay Sockeye Salmon - check with Mike (may be more than 1 stockid)
